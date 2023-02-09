2d2213495b5fa6f2397095c4c8556d4e3f503938
root <jondawson917@gmail.com>
2023-02-09 13:47:08 -0800

    Trying

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import {createRoot} from "react-dom/client";
 // import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 // import "@fortawesome/fontawesome-free/css/all.css";
 // import "./index.css";
-// import App from "./App";
+import App from "./App";
 
 import reportWebVitals from "./reportWebVitals";
 const root = createRoot(document.getElementById('root'));
