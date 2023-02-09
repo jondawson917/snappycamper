ec0aeaf882b96175df10113e4e81484a1f4e686e
root <jondawson917@gmail.com>
2023-02-09 10:39:10 -0800

    deploy to heroku

--- src/old/Routes/CompanyProvider.js
+++
@@ -1,8 +0,0 @@
-import React, {useState} from 'react';
-
-import CompanyContext from './CompanyContext';
-
-
-const CompanyProvider = ({children})=>{
-    const [company, setCompany] = useState([]);
-}
